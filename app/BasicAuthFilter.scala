1b49d3764bb75e64787dddc36b89c4cde2edb867
Kunio Sawatani <devel@fathens.org>
2015-02-19 22:05:27 +0900

    URL のチェックを正規表現に変更

--- app/BasicAuthFilter.scala
+++ app/BasicAuthFilter.scala
@@ -1,31 +1,41 @@
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.matching.Regex
 
 import play.api.Logger
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{ Filter, RequestHeader, Result, Results }
 
 import org.apache.commons.codec.binary.Base64
+import org.fathens.play.util.Exception.allCatch
 
 import service.Settings
 
 object BasicAuthFilter extends Filter {
-  case class Realm(name: String, url: String, username: String, password: String) {
-    def isMatch(request: RequestHeader) = {
-      url startsWith request.uri
-    }
-    def canAuthorize(request: RequestHeader): Boolean = {
+  case class Realm(name: String, pathRegex: Regex, username: String, password: String) {
+    def authorize(request: RequestHeader): Boolean = { // Does not care pathRegex
       decodeBasicAuth(request) match {
         case Some(c) => (c._1 == username && c._2 == password)
         case None    => false
       }
     }
   }
+  def isMatch(request: RequestHeader)(hasRegex: (Regex, _)): Boolean = {
+    val regex = hasRegex._1
+    val m = regex.findFirstIn(request.uri)
+    Logger debug f"BasicAuth: ${request} with ${regex}: $m"
+    m.isDefined
+  }
   private lazy val realms = {
-    Settings.BASIC_AUTH_REALMS.split("\\s+").map { key =>
-      def get(a: String) = Settings get f"BASIC_AUTH_REALM_${key}_${a}"
-      Realm(get("NAME"), get("URL"), get("USERNAME"), get("PASSWORD"))
-    }
+    Settings.BASIC_AUTH_REALMS.split("\\s+").toList.flatMap { key =>
+      def get(a: String) = Option(Settings get f"BASIC_AUTH_REALM_${key}_${a}").filter(_.length > 0)
+      for {
+        name <- get("NAME")
+        path <- get("PATH").flatMap(r => allCatch.opt(r.r))
+        username <- get("USERNAME")
+        password <- get("PASSWORD")
+      } yield Realm(name, path, username, password)
+    }.groupBy(_.pathRegex)
   }
   def decodeBasicAuth(request: RequestHeader): Option[(String, String)] = {
     for {
@@ -37,12 +47,15 @@ object BasicAuthFilter extends Filter {
     } yield (username, password)
   }
   def apply(nextFilter: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
-    realms.find(_ isMatch request) match {
+    realms.find(isMatch(request)) match {
       case None => nextFilter(request)
-      case Some(realm) => if (realm canAuthorize request) nextFilter(request) else Future {
-        val remote = request.headers.get("x-forwarded-for") getOrElse request.remoteAddress
-        Logger.warn(f"Login failure: from ${remote} to ${request.uri}")
-        Results.Unauthorized.withHeaders(("WWW-Authenticate" -> f"""Basic realm="${realm.name}""""))
+      case Some((_, list)) => list.find(_ authorize request) match {
+        case Some(_) => nextFilter(request)
+        case None => Future {
+          val remote = request.headers.get("x-forwarded-for") getOrElse request.remoteAddress
+          Logger.warn(f"Login failure: from ${remote} to ${request.uri}")
+          Results.Unauthorized.withHeaders(("WWW-Authenticate" -> f"""Basic realm="${list.head.name}""""))
+        }
       }
     }
   }
